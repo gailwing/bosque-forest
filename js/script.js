4532ca16b27e0aa604362192992c356c4716a776
gailwing <abeegailogunyemee@gmail.com>
2022-09-17 12:38:31 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const hero = document.getElementById("hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(hero);
